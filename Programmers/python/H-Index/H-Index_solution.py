eefd8d691f6628c885aaeed65592ca08d8be43e1
usbkeep <78432057+usbkeep@users.noreply.github.com>
2021-03-06 19:50:45 +0900

    Update H-Index_solution.py
    add .py file

--- Programmers/python/H-Index/H-Index_solution.py
+++ Programmers/python/H-Index/H-Index_solution.py
@@ -1,1 +1,15 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42747?language=python3
 
+
+def solution(citations):
+    answer = 0
+    citations.sort()
+    
+    length = len(citations)
+
+    for index in range(length):
+
+        if length - index <= citations[index]:
+            return length - index
+
+    return answer
