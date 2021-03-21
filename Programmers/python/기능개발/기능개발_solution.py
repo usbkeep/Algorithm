e3904d47cf82c33c67c62efa972b14c0b83a43fd
usbkeep <78432057+usbkeep@users.noreply.github.com>
2021-03-22 04:13:36 +0900

    Update 기능개발_solution.py

--- Programmers/python/기능개발/기능개발_solution.py
+++ Programmers/python/기능개발/기능개발_solution.py
@@ -1,1 +1,20 @@
+def solution(progresses, speeds):
 
+    answer = []
+    time = 1
+    complete = 0
+
+    while len(progresses) > 0:
+        temp = progresses[0] + (speeds[0] * time)
+
+        if temp >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            complete += 1
+        else:
+            if complete > 0:
+                answer.append(complete)
+                complete = 0
+            time += 1
+    answer.append(complete)
+    return answer
