101daa53fef3b3a5de80437bf3e36cacab36cb55
usbkeep <78432057+usbkeep@users.noreply.github.com>
2021-03-24 05:09:35 +0900

    Update 문자열_압축.py

--- kakao/2020_KAKAO_BLIND_RECRUITMENT/문자열_압축.py
+++ kakao/2020_KAKAO_BLIND_RECRUITMENT/문자열_압축.py
@@ -1,1 +1,30 @@
+def solution(s):
 
+    if (s_len := len(s)) <= 3:
+        return s_len
+
+    min_len = s_len
+    for step in range(1, s_len//2+1):  # step is "1 ~ len(s)//2"
+        test = ''
+        count = 1
+        current = s[:step]
+
+        for i in range(step, s_len, step):
+            if current == (next_s := s[i:i + step]):
+                count += 1
+            else:
+                if count > 1:
+                    test += str(count) + current
+                    count = 1
+                else:
+                    test += current
+            current = next_s
+        if count > 1:
+            test += str(count) + current
+        else:
+            test += current
+
+        if min_len > (test_len := len(test)):
+            min_len = test_len
+
+    return min_len
