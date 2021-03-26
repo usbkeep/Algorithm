72fe2e44d4b1f1f9899bb951da804a65d7254446
usbkeep <78432057+usbkeep@users.noreply.github.com>
2021-03-26 20:38:03 +0900

    Create 완주하지_못한_선수.java

---
+++ Programmers/완주하지_못한_선수/완주하지_못한_선수.java
@@ -0,0 +1,17 @@
+public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String p : participant) {
+        	map.put(p, map.getOrDefault(p, 0) +1);
+        }
+        
+        for(String c : completion) {
+        	if(map.get(c) == 1) {
+        		map.remove(c);
+        	}
+        	else {
+        		map.put(c, map.get(c) - 1);
+        	}
+        }
+        return map.keySet().iterator().next();
+    }
