9f77d639035275e48cba4a81cf2b6a354dedade3
usbkeep <usbkeep@gmail.com>
2021-12-17 22:06:36 +0900

    add 1001, 1260.py

---
+++ Beakjoon/1260.py
@@ -0,0 +1,72 @@
+# DFS와 BFS
+# https://www.acmicpc.net/problem/1260
+
+"""
+    첫째 줄에 정점의 개수 N(1 ≤ N ≤ 1,000),
+    간선의 개수 M(1 ≤ M ≤ 10,000),
+    탐색을 시작할 정점의 번호 V가 주어진다.
+    다음 M개의 줄에는 간선이 연결하는 두 정점의 번호가 주어진다.
+    어떤 두 정점 사이에 여러 개의 간선이 있을 수 있다.
+    입력으로 주어지는 간선은 양방향이다.
+
+"""
+
+import sys
+import collections
+
+
+graph = dict()
+
+# DFS
+def DFS(root):
+    visited = []
+    stack = [root]
+
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.append(node)
+            if graph.get(node):
+                stack.extend(sorted(list(graph[node]),reverse=True))
+
+    return visited
+
+# BFS
+def BFS(root):
+    visited = []
+
+    que = collections.deque([root])
+
+    while que:
+        node = que.popleft()
+        if node not in visited:
+            visited.append(node)
+            if graph.get(node):
+                que.extend(sorted(list(graph[node])))
+
+    return visited
+
+def print_visited(visited):
+
+    for v in visited:
+        print(f"{v}",end=' ')
+    print()
+    return
+
+
+N, M, V = map(int,sys.stdin.readline().split())
+
+for i in range(M):
+    vertex, edge = map(int,sys.stdin.readline().split())
+    if graph.get(vertex):
+        graph[vertex].add(edge)
+    else:
+        graph[vertex] = {edge}
+    if graph.get(edge):
+        graph[edge].add(vertex)
+    else:
+        graph[edge] = {vertex}
+
+
+print_visited(DFS(V))
+print_visited(BFS(V))
