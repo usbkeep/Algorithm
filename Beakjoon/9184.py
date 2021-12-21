43b2c77f2e0809fc1554244755a9dc6b821fc769
usbkeep <usbkeep@gmail.com>
2021-12-22 00:18:39 +0900

    edit 9184.py use (dict -> dp_array)

--- Beakjoon/9184.py
+++ Beakjoon/9184.py
@@ -6,23 +6,22 @@
 """
 import sys
 
-w_dict = dict()
+max_size = 21
+dp = [[[None for _ in range(max_size)]for _ in range(max_size)] for _ in range(max_size)]
 
 def w(a,b,c):
-
     if a <= 0 or b <= 0 or c <= 0:
         return 1
-    elif not w_dict.get((a,b,c)):
-        if a > 20 or b > 20 or c > 20:
-            w_dict[(a,b,c)] = w(20, 20, 20)
+    if a > 20 or b > 20 or c > 20:
+        return w(20,20,20)
+    if dp[a][b][c] is not None:
+        pass
+    elif a < b and b < c:
+        dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+    else:
+        dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
 
-        elif a < b and b < c:
-            w_dict[(a,b,c)] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
-
-        else:
-            w_dict[(a,b,c)] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
-
-    return w_dict[(a, b, c)]
+    return dp[a][b][c]
 
 while True:
     a, b, c = map(int, sys.stdin.readline().split())
